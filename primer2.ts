f6a66583221da4440f58fa570b7ce41e89c5ffaf
jagosmedenica <jagos.medenica.mne@gmail.com>
2024-04-04 15:35:41 +0200

    Neka poruka

---
+++ primer2.ts
@@ -0,0 +1,8 @@
+function proizvodDvaBrojaTS(a:number,b:number):number{
+    return a*b;
+}
+
+    let a:number=5;
+    let b:number=3;
+  let rezultatTS:number=proizvodDvaBrojaTS(aTs,bTs);
+  console.log("Proizvod dva broja sa ts je:", rezultatTs);
